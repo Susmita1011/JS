35d9d3a9ef35f825a0fb94b3b69c455676fc4e21
Susmita1011 <117312414+Susmita1011@users.noreply.github.com>
2025-06-14 09:31:09 +0000

    few points

---
+++ 01_JS/memory.js
@@ -0,0 +1,11 @@
+// Memory management in JS
+
+/*
+2 types data type: Primitive or Non primitive/reference type
+ there are 2 type of memory in work here:
+ 1. Stack   2. Heap
+
+ Primitive type datatype uses Stack memory --> from here we get the copy of variable
+ Non primitive type uses Heap memory --> from here we get the reference of the original variable
+
+*/
