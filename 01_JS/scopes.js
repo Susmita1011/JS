06dea50dac4246fe77168bb983aae7a0a5ffe912
Susmita1011 <117312414+Susmita1011@users.noreply.github.com>
2025-06-14 18:22:22 +0000

    scope

--- 01_JS/scopes.js
+++ 01_JS/scopes.js
@@ -13,5 +13,31 @@ d = 10 //scoped globally means scope unaware
 }
 
 // CORS or browser globalscope is different from the global scope of this nod scope
+// Nested scope
+/*
+function one(){
+   const username = "susmita"
+   function two(){
+     const wensite = "youtube"
+     console.log("username",username)
+   }
+     console.log("website",website) // console.log("website",website) // error website cant be accessed outside and two() never executed
+     two()
+}
+one()
 
+Parents variable can be accessed by child and not the vice versa
 
+*/
+
+// *******************Interesting**********************
+//console.log(printone(6)) //7 no error function is defined normally
+function printone(num){
+   return num + 1
+}
+// console.log(printone(5))  // 6
+//addTwo(6)  // error -> cant access addTwo before initialization when func declared as variable
+const addTwo = function(num){  // function declared as literal
+    return num+2
+}
+//addTwo(6) -> 8
