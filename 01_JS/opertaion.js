ad976b520af3be3c012e01b727c41a80719fc0ab
Susmita1011 <117312414+Susmita1011@users.noreply.github.com>
2025-06-14 08:43:40 +0000

    basic

---
+++ 01_JS/opertaion.js
@@ -0,0 +1,48 @@
+// Operations in JS
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(3/2); // float div
+// console.log(3%2);
+
+// console.log("helo"+" susmita");
+// console.log("helo"*3); //Nan
+
+// console.log('1'+2);//12
+// console.log(1 + '2');//12
+// console.log('1' + 2+2);//122
+// console.log(1+ 2+'2');//32
+// console.log(1+ '2'+2); //122
+
+// if str in first then it converts the other to str and at last first it executes firsts and then concatenate the last str
+
+// console.log(+true); // 1
+// console.log(true+); // error
+
+let n1,n2,n3
+
+// n1 = n2 = n3 = (2+2)
+
+let game = 100
+/*
+console.log(++game);
+console.log(game++);
+console.log(game);
+101
+101
+102
+*/
+
+
+
+
+
+
+
