732c3aa52b7697ffee8a1a6e1b1cbb6f93972df3
Susmita1011 <117312414+Susmita1011@users.noreply.github.com>
2025-06-16 09:28:12 +0000

    end

---
+++ 01_JS/basic.js
@@ -0,0 +1,19 @@
+//callback function dont have name 
+// For each with array
+
+const coding = ['js','py','Java','C','Cpp']
+//Iterating using for each 
+
+// coding.forEach(function (item) {console.log(item);}) // function here is call back function which has no name. each item of array you refer it as an item and provide a body to do something with item
+// coding.forEach(function (i) {console.log(i);}) // function here is call back function which has no name. each item of array you refer it as an item and provide a body to do something with item
+
+// Uisng arrow function
+//coding.forEach((item) => console.log(item))
+
+// function a(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(a) // just give the reference of function dont call it like a()
+
+//coding.forEach(item,index ,arr) //gets each item,index of it and whole arr
--- 01_JS/loops.js
+++ 01_JS/loops.js
@@ -1,99 +1,118 @@
-// for loops:
+// // for loops:
 
-// let array = [1,2,3,4,5,6]
+// // let array = [1,2,3,4,5,6]
 
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
-//     console.log(element);
+// // for (let index = 0; index < array.length; index++) {
+// //     const element = array[index];
+// //     console.log(element);
     
     
-// }
+// // }
 
 
 
-// While loop and do while:
+// // While loop and do while:
 
-// while (condition) {
-//     //code
-// }
+// // while (condition) {
+// //     //code
+// // }
 
-// let myArr = [1,2,3,4]
-// let i =0
-// while(i<myArr.length){
-//     console.log(myArr[i]);
-//     i+=1;
+// // let myArr = [1,2,3,4]
+// // let i =0
+// // while(i<myArr.length){
+// //     console.log(myArr[i]);
+// //     i+=1;
     
-// }
+// // }
 
 
 
 
-//do-while
-// let score = 6
-// do {
-//     console.log(score);
-//     score++
-// } while (score<=5);
+// //do-while
+// // let score = 6
+// // do {
+// //     console.log(score);
+// //     score++
+// // } while (score<=5);
 
-//Array specific loops
+// //Array specific loops
 
-// for of loop
+// // for of loop
 
-const arr = [1,2,3,4,5,6]
+// const arr = [1,2,3,4,5,6]
 
-// for (const num of arr) { similr to python default for good to get each of the value of array,string,object
-//     console.log(num);
+// // for (const num of arr) { similr to python default for good to get each of the value of array,string,object
+// //     console.log(num);
     
+// // }
+
+
+// // Maps: holds the key value pair and remembers the order of insertion. Unique value no duplicate value
+
+// const map = new Map()
+// map.set('IN','India')
+// map.set('US','United States of America')
+// map.set('FR','France')
+// //console.log(map);
+// /*
+// Map(3) {
+//   'IN' => 'India',
+//   'US' => 'United States of America',
+//   'FR' => 'France'
 // }
+//   */
 
-
-// Maps: holds the key value pair and remembers the order of insertion. Unique value no duplicate value
-
-const map = new Map()
-map.set('IN','India')
-map.set('US','United States of America')
-map.set('FR','France')
-//console.log(map);
-/*
-Map(3) {
-  'IN' => 'India',
-  'US' => 'United States of America',
-  'FR' => 'France'
-}
-  */
-
-// for (const [key,value] of map) {
-//   //  console.log(key); // [ 'IN', 'India' ],[ 'US', 'United States of America' ],[ 'FR', 'France' ]
-//     //console.log(key); // IN,US,FR
+// // for (const [key,value] of map) {
+// //   //  console.log(key); // [ 'IN', 'India' ],[ 'US', 'United States of America' ],[ 'FR', 'France' ]
+// //     //console.log(key); // IN,US,FR
     
     
-// }
+// // }
 
 
-//let myObj = {"IN":"India","US":"USA","FR":"France"}
-// for (const [key,value] of myObj) { // cant be deconstructured myObj is not iterable
-//     console.log(key);
-//     console.log(value);  
-    // Cant be iterated this way for object this way but Maps are itterable with for of loop [key,value]
-// }
-//-------------------------------------------------------------------------------
+// //let myObj = {"IN":"India","US":"USA","FR":"France"}
+// // for (const [key,value] of myObj) { // cant be deconstructured myObj is not iterable
+// //     console.log(key);
+// //     console.log(value);  
+//     // Cant be iterated this way for object this way but Maps are itterable with for of loop [key,value]
+// // }
+// //-------------------------------------------------------------------------------
 
-// let myObj = {IN:"India",US:"USA",FR:"France"}
+// // let myObj = {IN:"India",US:"USA",FR:"France"}
 
-// for (const key in myObj) {
-//     //console.log(key); // IN,US,FR
-//     //console.log(myObj[key]); // India, USA,France
+// // for (const key in myObj) {
+// //     //console.log(key); // IN,US,FR
+// //     //console.log(myObj[key]); // India, USA,France
     
-//     //for in works with the Object and for of in MAP
-// }
+// //     //for in works with the Object and for of in MAP
+// // }
 
-// let arr1 = ['js','java','py']
-// for (const key in arr1) {
-//    //console.log(key); // prints the index for the array
-//    console.log(arr1[key]);
+// // let arr1 = ['js','java','py']
+// // for (const key in arr1) {
+// //    //console.log(key); // prints the index for the array
+// //    console.log(arr1[key]);
    
-// }
-// for (const key in map) {
-//     console.log(key); // no output map is not itteratable
+// // }
+// // for (const key in map) {
+// //     console.log(key); // no output map is not itteratable
     
-// }
+// // }
+
+
+// // Reduce
+
+// const num = [1,2,3,4]
+
+// //Using normal function
+
+// // const total = num.reduce(function(acc,item){
+// //   console.log(`acc: ${acc} , currentItem: ${item}`);
+  
+// //   return acc+item
+// // },0) // array.reduce(function,accumulator value)
+
+// //Using arrow func
+// const total = num.reduce((item,acc) => {
+//  console.log(`acc: ${acc},item:${item}`)
+// return acc+item},0 )
+// console.log(total);
